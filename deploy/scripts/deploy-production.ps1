74ad551a012d1c1e051e47273b33c2fbbd1cd5c8
Markinatorina <markinatorina@hotmail.com>
2026-09-04 09:03:33 +0200

    feat(ops): make certificate deployment transactional

--- deploy/scripts/deploy-production.ps1
+++ deploy/scripts/deploy-production.ps1
@@ -245,6 +245,7 @@ fi
 if [ "`$activate_requested" = true ] || [ "`$was_active" = true ]; then
     /usr/local/sbin/activate-mail-stack
     /usr/local/lib/mk8email/tests/management_cli_smoke
+    /usr/local/lib/mk8email/tests/certificate_deploy_smoke
 fi
 apt_sources_changing=false
 "@
